f307260ecc3575d99f0995a71b0735bcebb46cfb
KDW79 <no1holycow@gmail.com>
2020-07-06 13:53:48 +0900

    17장 객체지향 실습

---
+++ Ch17_OOP/GreetingBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch17_OOP
+{
+    abstract class GreetingBase
+    {
+        public virtual string GetMessage()
+        {
+            return "";
+
+        }
+    }
+
+    class GreetingMorning : GreetingBase
+    {
+        public override string GetMessage()
+        {
+            return "Good Morning";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingMorning";
+        }
+    }
+
+    class GreetingAfternoon : GreetingBase
+    {
+        public override string GetMessage()
+        {
+            return "Good Afternoon";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingAfternoon";
+        }
+    }
+
+    class GreetingEvening : GreetingBase
+    {
+        public override string GetMessage()
+        {
+            return "Good Evening";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingEvening";
+        }
+    }
+
+    class GreetingMorning2 : IGreeting
+    {
+        public  string GetMessage()
+        {
+            return "Good Morning";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingMorning";
+        }
+    }
+
+    class GreetingAfternoon2 : IGreeting
+    {
+        public  string GetMessage()
+        {
+            return "Good Afternoon";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingAfternoon";
+        }
+    }
+
+    class GreetingEvening2 : IGreeting
+    {
+        public  string GetMessage()
+        {
+            return "Good Evening";
+        }
+        public override string ToString()
+        {
+            return "Class Name: GreetingEvening";
+        }
+    }
+
+}
---
+++ Ch17_OOP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch17_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greetings = new List<GreetingBase>()
+            {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+
+            foreach (var greeting in greetings)
+            {
+                Console.WriteLine(greeting.GetMessage());
+            }
+
+            var greetings2 = new List<IGreeting>()
+            {
+                new GreetingMorning2(),
+                new GreetingAfternoon2(),
+                new GreetingEvening2(),
+            };
+
+            foreach (var obj in greetings2)
+            {
+                Console.WriteLine(obj.GetMessage());
+            }
+
+
+        }
+    }
+}
